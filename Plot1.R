62f12971a8319f6ca665778ed30ac4158689f435
ksarpang <kishan.sarpangala@hotmail.com>
2018-05-06 23:27:03 -0400

    Summary
    This the repository for course Project-1 of Exploratory Data Analysis course offered by JHU

--- Plot1.R
+++ Plot1.R
@@ -9,7 +9,6 @@ names(household_power_file) <- c("Date","Time","Global_active_power","Global_rea
 
 sub_power <- subset(household_power_file,household_power_file$Date=="1/2/2007" | household_power_file$Date =="2/2/2007")
 
-#str(subSetData)
 globalActivePower <- as.numeric(household_power_file$Global_active_power)
 #Png file will be generated where ever you run this code. In the same directory
 png("plot1.png", width=480, height=480)
